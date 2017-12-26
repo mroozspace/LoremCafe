372907b92f456486d4267f8dbae6ccf2298a5c1d
mrooz <a.mrooz93@gmail.com>
2017-12-26 12:27:07 +0100

    contact - map added

--- dest/public/js/main.js
+++ dest/public/js/main.js
@@ -18,6 +18,17 @@ function fixedHeader(){
 	}
 
 }
+function initMap() {
+  var warsaw = {lat: 52.229852, lng: 21.011723};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 14,
+    center: warsaw
+  });
+    var marker = new google.maps.Marker({
+    position: warsaw,
+    map: map
+});
+}
 var slideIndex = 0;
 showSlides();
 
