90ef03e58278faf297936597b4c334fc8d995b23
mrooz <a.mrooz93@gmail.com>
2018-01-15 13:11:54 +0100

    attempt to preload

---
+++ src/public/js/loader.js
@@ -0,0 +1,16 @@
+var loader = document.getElementById("loader");
+var mainSlideShow = document.getElementById('slideShow');
+
+mainSlideShow.onload = closePreload();
+
+function closePreload(){
+
+	setTimeout(function(){
+		loader.classList.add("fadeOut");
+		document.querySelector('body').classList.add("no-scroll");
+	},2000);
+	setTimeout(function(){
+		loader.style.display=("none");
+		document.querySelector('body').classList.remove("no-scroll");
+	},4000);
+}
