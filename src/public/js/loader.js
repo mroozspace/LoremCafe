90f27a3ee8c7da7b06b2488bfc38ade2a070fcea
mrooz <a.mrooz93@gmail.com>
2018-01-15 22:18:35 +0100

    preload - 2nd attempt

--- src/public/js/loader.js
+++ src/public/js/loader.js
@@ -1,19 +1,19 @@
 var loader = document.getElementById("loader");
 var mainSlideShow = document.getElementById('slideShow');
 
+if(mainSlideShow){
 
-// if(mainSlideShow){
-// mainSlideShow.onload = closePreload();
+window.addEventListener('load',closePreload());
 
-// 	function closePreload(){
+	function closePreload(){
 
-// 		setTimeout(function(){
-// 			loader.classList.add("fadeOut");
-// 			document.querySelector('body').classList.add("no-scroll");
-// 		},2000);
-// 		setTimeout(function(){
-// 			loader.style.display=("none");
-// 			document.querySelector('body').classList.remove("no-scroll");
-// 		},4000);
-// 	}
-// }
+		setTimeout(function(){
+			loader.classList.add("fadeOut");
+			document.querySelector('body').classList.add("no-scroll");
+		},2000);
+		setTimeout(function(){
+			loader.style.display=("none");
+			document.querySelector('body').classList.remove("no-scroll");
+		},4000);
+	}
+}
