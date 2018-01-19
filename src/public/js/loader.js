9df2c4d3065905ac393369e9beea8af30f1f3543
mrooz <a.mrooz93@gmail.com>
2018-01-19 14:36:28 +0100

    change of loader style. Deleted loader comments from htmls

--- src/public/js/loader.js
+++ src/public/js/loader.js
@@ -2,37 +2,33 @@ var loader = document.getElementById("loader");
 var mainSlideShow = document.getElementById('slideShow');
 var images = document.getElementsByTagName("img");
 
-
 document.onreadystatechange = function(){
 	switch(document.readyState){
 		case "loading":
-			console.log('loading DOM');
+			// console.log('loading DOM');
 			setTimeout(function(){
-				console.log('loading + timeout');
+				// console.log('loading + timeout');
 			},1000);
-
 		break;
 
 		case "interactive":
-			console.log('Doc loaded. Changing DOM');
+			// console.log('Doc loaded. Changing DOM');
 			setTimeout(function(){
-				console.log('doc loaded + timeout');
+				// console.log('doc loaded + timeout');
 			},2000);
 		break;
 
 		case "complete":
-			console.log('Doc complete');
-			
+			// console.log('Doc complete');
 			setTimeout(function(){
 			loader.classList.add("fadeOut");
-			console.log('starting fadeout');
-			},2000);
+			// console.log('starting fadeout');
+			},2200);
 			
 			setTimeout(function(){
-			console.log('disappear + timeout');
+			// console.log('disappear + timeout');
 			loader.style.display = "none"
 			},5000);
-			
 		break;
 	}
 };
