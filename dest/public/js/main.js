7104d3c41a246c9a367944c1cb3edf7a2ec99e92
mrooz <a.mrooz93@gmail.com>
2018-01-19 21:18:24 +0100

    =>BEM - header on index page

--- dest/public/js/main.js
+++ dest/public/js/main.js
@@ -3,31 +3,8 @@
 
 var body = document.getElementsByTagName("body")[0];
 var nav = document.getElementsByTagName("nav")[0];
-// var scrollPos = document.documentElement.scrollTop || document.body.scrollTop;
-
 
 window.onscroll = function(){fixedHeader()};
-body.onresize = function(){fixedOnResize()};
-
-fixedOnResize();
-
-function fixedOnResize(){
-
-	var width = window.innerWidth
-	|| document.documentElement.clientWidth
-	|| document.body.clientWidth;
-
-	if(width<=768){
-		header.classList.add("fixed-header");
-		nav.style.display = "block";
-	} 
-	if(width>768){
-		header.classList.remove("fixed-header");
-		nav.style.display = "none";
-	}
-
-	// console.log(width);
-}
 
 function fixedHeader(){
 
@@ -37,13 +14,11 @@ function fixedHeader(){
 
 	var scrollPos = document.documentElement.scrollTop || document.body.scrollTop;
 	var header = document.getElementById("header");
-	console.log(scrollPos);
-	if(width>768){
-		if(scrollPos > 230){
-			header.classList.add("fixed-header");
-		} else {
-			header.classList.remove("fixed-header");
-		}
+	// console.log(scrollPos);
+	if(scrollPos > 230){
+		header.classList.add("header--fixed");
+	} else {
+		header.classList.remove("header--fixed");
 	}
 }
 
